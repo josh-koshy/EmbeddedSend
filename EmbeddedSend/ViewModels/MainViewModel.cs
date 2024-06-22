91ae9f4c70a32d3c3883ee12f2713beed833737c
Joshua Koshy <89991793+josh-koshy@users.noreply.github.com>
2024-06-22 12:12:17 -0400

    added a settings view

--- EmbeddedSend/ViewModels/MainViewModel.cs
+++ EmbeddedSend/ViewModels/MainViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿// "(C) 2024 Joshua Koshy."
+
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +10,7 @@ using System.Windows.Input;
 using CommunityToolkit.Mvvm;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using EmbeddedSend.Views;
 
 namespace EmbeddedSend.ViewModels
 {
@@ -15,13 +19,28 @@ namespace EmbeddedSend.ViewModels
 		private string _testString = "Hello, World.";
         private int _testStringInt = 0;
 		public ICommand ButtonPress { get; }
+        public ICommand SettingsMenuPress { get; }
+        public ICommand AboutMenuPress { get; }
 
         public MainViewModel()
         {
             ButtonPress = new RelayCommand(IncrementHelloWorldNumber);
+            SettingsMenuPress = new RelayCommand(OnSettingsMenuPress);
+            AboutMenuPress = new RelayCommand(OnAboutMenuPress);
         }
 
 
+
+        public void OnSettingsMenuPress()
+        {
+            Debug.WriteLine("Settings Menu Pressed.");
+            SettingsWindow settingsWindow = new SettingsWindow();
+            settingsWindow.ShowDialog();
+        }
+        public void OnAboutMenuPress()
+        {
+            Debug.WriteLine("About Menu Pressed");
+        }
         private void IncrementHelloWorldNumber()
         {
             _testStringInt++;
